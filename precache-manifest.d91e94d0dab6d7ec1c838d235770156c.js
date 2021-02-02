07995194060d299a2e21fedaebbdc2fcc5098d34
veedee <43088920+veedee2000@users.noreply.github.com>
2021-02-02 17:41:03 +0530

    Updates

---
+++ precache-manifest.d91e94d0dab6d7ec1c838d235770156c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a48c139c0942249230a0",
+    "url": "/static/css/main.4ed0ff26.chunk.css"
+  },
+  {
+    "revision": "a48c139c0942249230a0",
+    "url": "/static/js/main.a48c139c.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "a29d401df038621480e4395028a24c24",
+    "url": "/index.html"
+  }
+];
